84a15bb46006bfd46f783f4677bc0ac8f0eb11b2
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-09-20 10:44:32 +0700

    Creating Home Layout and the list item note

--- app/src/main/java/com/aminivan/mynotes/fragment/FragmentHome.kt
+++ app/src/main/java/com/aminivan/mynotes/fragment/FragmentHome.kt
@@ -6,17 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.aminivan.mynotes.R
+import com.aminivan.mynotes.databinding.FragmentHomeBinding
+import com.aminivan.mynotes.databinding.FragmentLoginBinding
 
 class FragmentHome : Fragment() {
 
+    lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view }
 
 
 }
--- app/src/main/java/com/aminivan/mynotes/fragment/FragmentLogin.kt
+++ app/src/main/java/com/aminivan/mynotes/fragment/FragmentLogin.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.aminivan.mynotes.R
 import com.aminivan.mynotes.databinding.FragmentLoginBinding
 import com.aminivan.mynotes.databinding.FragmentSplashBinding
@@ -29,6 +30,15 @@ class FragmentLogin : Fragment() {
         Glide.with(this)
             .load(R.drawable.login)
             .into(binding.ivLogin);
+
+        binding.btnLogin.setOnClickListener(){
+            gotoHome()
+        }
+
+    }
+
+    fun gotoHome(){
+        Navigation.findNavController(requireView()).navigate(R.id.action_fragmentLogin_to_fragmentHome)
     }
 
 }
