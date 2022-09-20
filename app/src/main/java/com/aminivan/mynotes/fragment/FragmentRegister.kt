9e6fa4b4e50701b7117294e452cf03d5b1a3e3fb
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-09-20 10:04:04 +0700

    Initial commit

---
+++ app/src/main/java/com/aminivan/mynotes/fragment/FragmentRegister.kt
@@ -0,0 +1,31 @@
+package com.aminivan.mynotes.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aminivan.mynotes.R
+import com.aminivan.mynotes.databinding.FragmentRegisterBinding
+import com.bumptech.glide.Glide
+
+
+class FragmentRegister : Fragment() {
+    lateinit var binding : FragmentRegisterBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+            .load(R.drawable.document)
+            .into(binding.ivSplash);
+    }
+
+}
