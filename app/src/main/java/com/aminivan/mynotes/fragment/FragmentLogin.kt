bd1f3f2e133ef38c0c5e0db490886073d87d6464
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-09-20 10:26:41 +0700

    Creating Login Layout

--- app/src/main/java/com/aminivan/mynotes/fragment/FragmentLogin.kt
+++ app/src/main/java/com/aminivan/mynotes/fragment/FragmentLogin.kt
@@ -6,17 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.aminivan.mynotes.R
+import com.aminivan.mynotes.databinding.FragmentLoginBinding
+import com.aminivan.mynotes.databinding.FragmentSplashBinding
+import com.bumptech.glide.Glide
 
 
 class FragmentLogin : Fragment() {
 
+    lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+            .load(R.drawable.login)
+            .into(binding.ivLogin);
     }
 
 }
